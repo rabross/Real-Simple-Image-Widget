b535391a7f613cbd55f62054666c39e01c42db08
rabross <rab@rabross.com>
2019-04-23 10:18:58 +0100

    Fixed loading bug Updated Glide

--- app/src/main/java/com/rabross/android/realsimpleimagewidget/RemoteViewAdapter.kt
+++ app/src/main/java/com/rabross/android/realsimpleimagewidget/RemoteViewAdapter.kt
@@ -5,17 +5,32 @@ import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Point
 import android.net.Uri
 import android.view.View
+import android.view.WindowManager
 import android.widget.RemoteViews
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.AppWidgetTarget
 import com.bumptech.glide.request.target.Target
 
 class RemoteViewAdapter(val context: Context) {
 
+    private val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
     private lateinit var remoteViews: RemoteViews
+    private var width: Int = 0
+    private var height: Int = 0
+
+    init {
+        val size = Point()
+        windowManager.defaultDisplay.getSize(size)
+        width = size.x
+        height = size.y
+    }
 
     fun createView(): RemoteViews {
         remoteViews = RemoteViews(context.packageName, R.layout.widget)
@@ -28,22 +43,24 @@ class RemoteViewAdapter(val context: Context) {
     }
 
     private fun bindUri(appWidgetId: Int, uri: Uri) {
-        Glide.with(context)
-                .load(uri)
-                .asBitmap()
-                .fitCenter()
-                .listener(object : RequestListener<Uri, Bitmap> {
-                    override fun onException(e: Exception, model: Uri, target: Target<Bitmap>, isFirstResource: Boolean): Boolean {
-                        remoteViews.setTextViewText(R.id.widget_text, context.getString(R.string.error_failed))
-                        return false
-                    }
 
-                    override fun onResourceReady(resource: Bitmap, model: Uri, target: Target<Bitmap>, isFromMemoryCache: Boolean, isFirstResource: Boolean): Boolean {
+        Glide.with(context.applicationContext)
+                .asBitmap()
+                .override(width, height)
+                .load(uri)
+                .fitCenter()
+                .listener(object : RequestListener<Bitmap> {
+                    override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
                         remoteViews.setViewVisibility(R.id.widget_text, View.GONE)
                         return false
                     }
+
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                        remoteViews.setTextViewText(R.id.widget_text, context.getString(R.string.error_failed))
+                        return false
+                    }
                 })
-                .into(AppWidgetTarget(context, remoteViews, R.id.widget_image, appWidgetId))
+                .into(AppWidgetTarget(context, R.id.widget_image, remoteViews, appWidgetId))
     }
 
     private fun bindPendingIntent(appWidgetId: Int) {
